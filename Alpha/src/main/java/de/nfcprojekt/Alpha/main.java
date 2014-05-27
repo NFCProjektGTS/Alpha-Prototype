7f77d608871fe52448c405db828ebbfa86066073
kernproblem <kernproblem@live.de>
2014-05-27 16:29:40 +0200

    first commit

---
+++ Alpha/src/main/java/de/nfcprojekt/Alpha/main.java
@@ -0,0 +1,15 @@
+package de.nfcprojekt.Alpha;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
